0af9fc309be0a86d9c977246bfb14feba65c122f
andyexeter <palmer.andy@gmail.com>
2018-02-11 13:40:15 +0000

    Update UMD for webpack support
    Webpack uses the AMD part of our UMD and the global object wasn't being passed to the factory

--- dist/most-visible.js
+++ dist/most-visible.js
@@ -9,13 +9,15 @@
     /* jshint strict:false */
     /* global define: false, module: false */
     if (typeof define === 'function' && define.amd) {
-        define([], factory);
+        define([], function () {
+            return factory(root);
+        });
     } else if (typeof module === 'object' && module.exports) {
         module.exports = factory(root);
     } else {
         root.mostVisible = factory(root);
     }
-}(this, function (window) {
+}(typeof self !== 'undefined' ? self : this, function (window) {
     /* jshint unused: vars */
 
     'use strict';
